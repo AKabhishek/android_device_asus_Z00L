39d3e822ba95ca8120955a9cda65299acf19c9c6
AKabhishek <abhishekkaushik.star@gmail.com>
2016-10-12 15:19:09 +0000

    Bringing up AICP-12 For Z00L
    Change-Id: I4b23d948431e9dce0f3121effe8ec7f40a314035

--- aicp.mk
+++ aicp.mk
@@ -1,9 +1,9 @@
 $(call inherit-product, device/asus/Z00L/full_Z00L.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/aicp/config/common.mk)
 
-PRODUCT_NAME := cm_Z00L
+PRODUCT_NAME := aicp_Z00L
 PRODUCT_BRAND := asus
 PRODUCT_MANUFACTURER := asus
 PRODUCT_DEVICE := Z00L
